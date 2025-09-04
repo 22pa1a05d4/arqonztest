ef5db4ae637541088447eb734c6f2b92886f5bcb
22pa1a05d4 <22pa1a05d4@vishnu.edu.in>
2025-09-05 00:15:42 +0530

    check it

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.use('*', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0" ,() => {
   console.log(`Server running on port ${PORT}`);
   console.log(`Environment: ${process.env.NODE_ENV || 'development'}`);
   console.log(`Database: ${process.env.MONGODB_URI ? 'Atlas' : 'Local'}`);
